dfe912c06df10c944a56ac15b7b061d97c885c5c
javasabmat <javasabmat@JEDLAB-015>
2017-04-22 11:06:03 -0300

    laboratorio 02 22/04/2017

---
+++ Livraria/Livraria-Servico/src/main/java/dao/Pesquisa.java
@@ -0,0 +1,20 @@
+package dao;
+import java.util.Optional;
+import dao.LivroDao;
+
+public class Pesquisa {
+	
+	public static void main(String[] args){
+		
+		String titulo =
+	    Optional.of(args)
+	    .filter(p -> p.length>0)
+	    .get()[0];
+		
+		LivroDao dao = new LivroDao();
+		dao.consultar(titulo)
+		.forEach(p -> System.out.println("Desc: "+ p.getTitulo()
+		+ "  Preço: "+ p.getPreco()));
+	}
+
+}
